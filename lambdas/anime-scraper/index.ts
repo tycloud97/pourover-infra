c3c62becd87db68b4d070ec2d7889ad2dacd72b7
Isaiah Fielder <isaiahmfielder@gmail.com>
2021-06-18 12:02:55 -0600

    cleanup lambda

--- lambdas/anime-scraper/index.ts
+++ lambdas/anime-scraper/index.ts
@@ -3,14 +3,14 @@ import { Document } from "dynamoose/dist/Document";
 
 const schema = new dynamoose.Schema(
   {
-    pk: {
+    PK: {
       type: Object,
       hashKey: true,
       //@ts-expect-error
       set: (val) => `${val.entity}#${val.id}`,
       // "get": (val) =>  // split the string on # and return an object
     },
-    sk: {
+    SK: {
       type: Object,
       rangeKey: true,
       //@ts-expect-error
@@ -26,16 +26,20 @@ const schema = new dynamoose.Schema(
 );
 
 class AnimeEntity extends Document {
-  pk: { id: string; entity: string };
-  sk: { id: string; entity: string };
+  PK: { id: string; entity: string };
+  SK: { id: string; entity: string };
 }
 export const handler = async (event: { title: string; id: string }) => {
-  const AnimeEntity = dynamoose.model<AnimeEntity>("anime", schema);
-
-  await AnimeEntity.create({
-    pk: { id: event.id, entity: "ANIME" },
-    sk: { id: "v1", entity: "VERSION" },
+  //
+  const AnimeEntity = dynamoose.model<AnimeEntity>("anime", schema, {
+    create: false,
   });
+
+  const anime = new AnimeEntity({
+    PK: { id: event.id, entity: "ANIME" },
+    SK: { id: "v1", entity: "VERSION" },
+  });
+  await anime.save().catch((err) => console.error(err));
 
   return;
 };
